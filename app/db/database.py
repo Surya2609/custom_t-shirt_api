343aca624501e82255693d8dc9654ee4226d9ae8
Jayasurya <a.jayasuryamct2019@gmail.com>
2025-09-17 10:12:38 -0700

    db connection chagne

--- app/db/database.py
+++ app/db/database.py
@@ -2,11 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+mysqlconnector://sql12797955:PXSPlB75N3@sql12.freesqldatabase.com:3306/sql12797955"
-
-
-
-# DATABASE_URL = "mysql+mysqlconnector://root:Surya%4026@localhost:3306/tshirt_store"
+# ✅ New Clever-Cloud DB URL
+DATABASE_URL = (
+    "mysql+mysqlconnector://uafweadkhrfidg6l:egPFBXbQSHHHXUnZYzU1"
+    "@bv7b5zx4lhfaorgyrwbv-mysql.services.clever-cloud.com:3306/bv7b5zx4lhfaorgyrwbv"
+)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
